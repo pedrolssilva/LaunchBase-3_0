aa894dff9b46ff152a0b644f5c15762ba242e031
pedrolssilva <pedrolssilva@hotmail.com>
2020-08-18 19:57:19 -0300

    concluded teaching about Module04 - dinamic data

---
+++ modulo04/data.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 minutos",
+    price: "Free",
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na prática | Mayk Brito",
+    duration: "36 minutos",
+    price: "Free",
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: Higher Order Function | Mayk Brito",
+    duration: "54 minutos",
+    price: "Free",
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O que é API? REST e RESTful?| Mayk Brito",
+    duration: "33 minutos",
+    price: "Free",
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a variável this no JavaScript | Mayk Brito",
+    duration: "48 minutos",
+    price: "Free",
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title:
+      "Como usar o Git e GitHub na Prática: Guia para iniciantes | Mayk Brito",
+    duration: "33 minutos",
+    price: "Free",
+  },
+];
--- modulo04/server.js
+++ modulo04/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nunjuncks = require("nunjucks");
 
 const server = express();
+const videos = require("./data");
 
 server.use(express.static("public"));
 
@@ -9,14 +10,37 @@ server.set("view engine", "njk");
 
 nunjuncks.configure("views", {
   express: server,
+  autoescape: false,
 });
 
 server.get("/", function (req, res) {
-  return res.render("about");
+  const about = {
+    avatar_irl:
+      "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    name: "Mayk Brito",
+    role: "Instrutor - Rocketseat",
+    description:
+      'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a target="_blank" href="https://rocketseat.com.br">Rocketseat</a> ',
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/maykbrito/",
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/maykbrito/",
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/maykbrito/",
+      },
+    ],
+  };
+  return res.render("about", { about });
 });
 
 server.get("/portfolio", function (req, res) {
-  return res.render("portfolio");
+  return res.render("portfolio", { items: videos });
 });
 
 server.listen(5000, function () {
