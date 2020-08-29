a4a868e471f3fec33e6528bd26aa8af2604b820f
pedrolssilva <pedrolssilva@hotmail.com>
2020-08-29 18:48:36 -0300

    Finished challenge 4-3

--- modulo05/Desafio-4_1-2-3/server.js
+++ modulo05/Desafio-4_1-2-3/server.js
@@ -3,6 +3,7 @@ const nunjuncks = require("nunjucks");
 const routes = require("./routes");
 const server = express();
 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
 server.use(routes);
 
@@ -13,10 +14,6 @@ nunjuncks.configure("views", {
   autoescape: false,
   noCache: true,
 });
-
-// server.use(function (req, res) {
-//   res.status(404).render("not-found");
-// });
 
 server.listen(5000, function () {
   console.log("Server is running");
