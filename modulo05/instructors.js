6e02aeb283510fa4b2f49335ba2b32e71f8ecf1c
pedrolssilva <pedrolssilva@hotmail.com>
2020-08-30 20:51:37 -0300

    woeking with parse of dates, created edit page, reusing code with njk include to use the same fields in create and edit page of instructor and learning about req.param to get data

--- modulo05/instructors.js
+++ modulo05/instructors.js
@@ -1,5 +1,30 @@
 const fs = require("fs");
 const data = require("./data.json");
+const { age, date } = require("./utils");
+
+//show
+exports.show = function (req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) {
+    return res.send("Instructor not found!");
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.trim().split(","),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(
+      foundInstructor.created_at
+    ),
+  };
+
+  return res.render("instructors/show", { instructor });
+};
 
 // create
 exports.post = function (req, res) {
@@ -32,4 +57,24 @@ exports.post = function (req, res) {
 
     return res.redirect("/instructors");
   });
+};
+
+//edit
+exports.edit = function (req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) {
+    return res.send("Instructor not found!");
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth),
+  };
+
+  return res.render("instructors/edit", { instructor });
 };
