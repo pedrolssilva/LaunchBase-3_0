7af8ebe526c641e5a6c316418409cb3f3a13d2bf
pedrolssilva <pedrolssilva@hotmail.com>
2020-10-22 21:45:28 -0300

    Foodfy Challenge => used dinamic data in recipes and about pages

---
+++ modulo05/FoodfyChallenge/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: 1,
+    image_url: "assets/burger.png",
+    foodPlate: "Triplo bacon burger",
+    chef: "Jorge Relato",
+  },
+  {
+    id: 2,
+    image_url: "assets/pizza.png",
+    foodPlate: "Pizza 4 estações",
+    chef: "Fabiana Melo",
+  },
+  {
+    id: 3,
+    image_url: "assets/espaguete.png",
+    foodPlate: "Espaguete de alho",
+    chef: "Júlia Kinoto",
+  },
+  {
+    id: 4,
+    image_url: "assets/lasanha.png",
+    foodPlate: "Lasanha mac n' cheese",
+    chef: "Juliano Vieira",
+  },
+  {
+    id: 5,
+    image_url: "assets/doce.png",
+    foodPlate: "Docinhos pão-do-céu",
+    chef: "Ricardo Golvea",
+  },
+  {
+    id: 5,
+    image_url: "assets/asinhas.png",
+    foodPlate: "Asinhas de frango ao barbecue",
+    chef: "Vania Steroski",
+  },
+];
--- modulo05/FoodfyChallenge/server.js
+++ modulo05/FoodfyChallenge/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nunjuncks = require("nunjucks");
 
 const server = express();
+const recipes = require("./data");
 
 server.use(express.static("public"));
 
@@ -18,11 +19,32 @@ server.get("/", function (req, res) {
 });
 
 server.get("/about", function (req, res) {
-  res.render("general/about");
+  const data = {
+    title: "Sobre o Foodfy",
+    contents: [
+      {
+        paragraphTitle: "",
+        description:
+          "Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue.<br/><br/>Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.",
+      },
+      {
+        paragraphTitle: "Como tudo começou...",
+        description:
+          "Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue.<br/><br/>Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.",
+      },
+      {
+        paragraphTitle: "Nossas receitas",
+        description:
+          "Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.",
+      },
+    ],
+  };
+
+  res.render("general/about", { data });
 });
 
 server.get("/recipes", function (req, res) {
-  res.render("recipes/recipes");
+  res.render("recipes/recipes", { recipes });
 });
 
 server.listen(5000, function () {
