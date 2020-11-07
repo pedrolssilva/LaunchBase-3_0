b87873fcb3dacb22b1e132434b0cee30c0013d80
pedrolssilva <pedrolssilva@hotmail.com>
2020-11-06 22:36:06 -0300

    Foodfy Challenge => adjusted edit.njk, using method-override and create back-end to edit recipe

--- modulo05/FoodfyChallenge/controllers/admin.js
+++ modulo05/FoodfyChallenge/controllers/admin.js
@@ -70,3 +70,43 @@ exports.show = function (req, res) {
 
   res.render("admin/show", { recipe: foundRecipe });
 };
+
+//put
+exports.put = function (req, res) {
+  const { id } = req.body;
+
+  let index = 0;
+
+  const foundRecipe = data.recipes.find(function (recipe, foundIndex) {
+    if (recipe.id == id) {
+      index = foundIndex;
+      return true;
+    }
+  });
+
+  if (!foundRecipe) {
+    return res.send("Recipe not found!");
+  }
+
+  const recipe = {
+    ...foundRecipe,
+    ...req.body,
+    ingredients: req.body.ingredients.filter((ingredient) => {
+      return ingredient != "" && ingredient != null;
+    }),
+    prepareMode: req.body.prepareMode.filter((prMode) => {
+      return prMode != "" && prMode != null;
+    }),
+    id: Number(id),
+  };
+
+  data.recipes[index] = recipe;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+      return res.send("Write error");
+    }
+
+    return res.redirect(`/admin/recipes/${id}`);
+  });
+};
