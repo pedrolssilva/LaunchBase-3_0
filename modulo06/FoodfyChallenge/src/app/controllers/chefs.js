c38ba87cfc4f617c886f57fea01d0faaeaf777f6
pedrolssilva <pedrolssilva@hotmail.com>
2021-02-09 21:43:44 -0300

    Foodfy challenge => removed console.log

--- modulo06/FoodfyChallenge/src/app/controllers/chefs.js
+++ modulo06/FoodfyChallenge/src/app/controllers/chefs.js
@@ -2,7 +2,6 @@ const Chef = require("../models/chef");
 
 exports.index = function (req, res) {
   Chef.all(function (chefs) {
-    console.log(chefs);
     return res.render("chefs/chefs", { chefs });
   });
 };
