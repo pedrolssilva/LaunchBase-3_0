d0f9fc3aa569543d267b7f6404e292c43f6a8fef
pedrolssilva <pedrolssilva@hotmail.com>
2021-04-14 20:10:57 -0300

    Module08 => The module that manages images in the front-end has been concluded.

--- modulo08/public/scripts.js
+++ modulo08/public/scripts.js
@@ -15,16 +15,21 @@ const Mask = {
 };
 
 const PhotosUpload = {
+  input: "",
   preview: document.querySelector("#photos-preview"),
   uploadLimit: 6,
+  files: [],
   handleFileInput(event) {
     const { files: fileList } = event.target;
+    PhotosUpload.input = event.target;
 
     if (PhotosUpload.hasLimit(event)) {
       return;
     }
 
     Array.from(fileList).forEach((file) => {
+      PhotosUpload.files.push(file);
+
       const reader = new FileReader();
 
       reader.onload = () => {
@@ -37,10 +42,12 @@ const PhotosUpload = {
 
       reader.readAsDataURL(file);
     });
+
+    PhotosUpload.input.files = PhotosUpload.getAllFiles();
   },
   hasLimit(event) {
-    const { uploadLimit } = PhotosUpload;
-    const { files: fileList } = event.target;
+    const { uploadLimit, input, preview } = PhotosUpload;
+    const { files: fileList } = input;
 
     if (fileList.length > uploadLimit) {
       alert(`Envie no máximo ${this.uploadLimit} fotos`);
@@ -48,7 +55,27 @@ const PhotosUpload = {
       return true;
     }
 
+    const photosDiv = [];
+    preview.childNodes.forEach((item) => {
+      if (item.classList && item.classList.value == "photo") {
+        photosDiv.push(item);
+      }
+    });
+
+    const totalPhotos = fileList.length + photosDiv.length;
+    if (totalPhotos > PhotosUpload.uploadLimit) {
+      alert("Você atingiu o limite máximo de fotos");
+      event.preventDefault();
+      return true;
+    }
     return false;
+  },
+  getAllFiles() {
+    const dataTransfer =
+      new ClipboardEvent("").clipboardData || new DataTransfer();
+
+    PhotosUpload.files.forEach((file) => dataTransfer.items.add(file));
+    return dataTransfer.files;
   },
   getContainer(image) {
     const div = document.createElement("div");
@@ -69,9 +96,12 @@ const PhotosUpload = {
     return button;
   },
   removePhoto(event) {
-    const photoDiv = event.target.parentNode;
+    const photoDiv = event.target.parentNode; // <div class="photo">
     const photosArray = Array.from(PhotosUpload.preview.children);
     const index = photosArray.indexOf(photoDiv);
+
+    PhotosUpload.files.splice(index, 1);
+    PhotosUpload.input.files = PhotosUpload.getAllFiles();
     photoDiv.remove();
   },
 };
