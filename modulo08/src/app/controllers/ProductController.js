de10131801b2a5f6e2fa72bf6a7b68ca7ca3e9d6
pedrolssilva <pedrolssilva@hotmail.com>
2021-04-15 19:49:18 -0300

    Module08 - image upload => file register finished in the back-end.

--- modulo08/src/app/controllers/ProductController.js
+++ modulo08/src/app/controllers/ProductController.js
@@ -1,6 +1,7 @@
 const { formatPrice } = require("../../lib/utils");
 const Category = require("../models/Category");
 const Product = require("../models/Product");
+const File = require("../models/File");
 
 module.exports = {
   create(req, res) {
@@ -22,10 +23,20 @@ module.exports = {
       }
     }
 
+    if (req.files.length == 0) {
+      return res.send("Please, send at least one image");
+    }
+
     let results = await Product.create(req.body);
     const productId = results.rows[0].id;
 
-    return res.redirect(`products/${productId}`);
+    const filesPromise = req.files.map((file) =>
+      File.create({ ...file, product_id: productId })
+    );
+
+    await Promise.all(filesPromise);
+
+    return res.redirect(`products/${productId}/edit`);
   },
   async edit(req, res) {
     let results = await Product.find(req.params.id);
