94c0a8b5b5984fc5a50501c7a8b0abac7a204533
pedrolssilva <pedrolssilva@hotmail.com>
2021-04-29 20:24:15 -0300

    Module09 => showing Launchstore products => Homepage => Concluded homepage with last products added

--- modulo09/src/app/controllers/HomeController.js
+++ modulo09/src/app/controllers/HomeController.js
@@ -1,6 +1,5 @@
 const { formatPrice } = require("../../lib/utils");
 const Product = require("../models/Product");
-const File = require("../models/File");
 
 module.exports = {
   async index(req, res) {
@@ -13,23 +12,28 @@ module.exports = {
 
     async function getImage(productId) {
       let results = await Product.files(productId);
-      const files = results.rows.map((file) => `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`);
+      const files = results.rows.map(
+        (file) =>
+          `${req.protocol}://${req.headers.host}${file.path.replace(
+            "public",
+            ""
+          )}`
+      );
 
       return files[0];
     }
 
-    const productsPromise = products.map(product => {
-      product.img = await getImage(product.id)
-      product.oldPrice = formatPrice(product.old_price)
-      product.price = formatPrice(product.price) 
-      return product
-    }).filter((product, index) =>  index > 2 ? false : true)
+    const productsPromise = products
+      .map(async (product) => {
+        product.img = await getImage(product.id);
+        product.oldPrice = formatPrice(product.old_price);
+        product.price = formatPrice(product.price);
+        return product;
+      })
+      .filter((product, index) => (index > 2 ? false : true));
 
     const lastAdded = await Promise.all(productsPromise);
 
-    return res.render("home/index", {products: lastAdded})
+    return res.render("home/index", { products: lastAdded });
   },
 };
