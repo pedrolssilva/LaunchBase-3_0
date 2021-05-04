d98a888759afd446f00238e1ae0b231dc6a02f2c
pedrolssilva <pedrolssilva@hotmail.com>
2021-05-04 20:30:20 -0300

    Module09 => showing Launchstore products => Search page => Concluded search page  with  front and backend implementations.

--- modulo09/src/app/models/Product.js
+++ modulo09/src/app/models/Product.js
@@ -76,4 +76,32 @@ module.exports = {
       [id]
     );
   },
+  search(params) {
+    const { filter, category } = params;
+
+    let query = "",
+      filterQuery = "WHERE";
+
+    if (category) {
+      filterQuery = `${filterQuery} 
+      products.category_id = ${category}
+      AND `;
+    }
+
+    filterQuery = `
+      ${filterQuery} 
+      products.name ilike '%${filter}%'
+      OR products.description ilike '%${filter}%'
+    `;
+
+    query = `
+      SELECT products.*,
+        categories.name AS category_name
+      FROM products
+      LEFT JOIN categories ON (categories.id = products.category_id)
+      ${filterQuery}
+      `;
+
+    return db.query(query);
+  },
 };
