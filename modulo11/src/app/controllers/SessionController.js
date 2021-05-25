16b33e8a715c994b5adee645340b5fb8384e6754
pedrolssilva <pedrolssilva@hotmail.com>
2021-05-24 21:27:50 -0300

    Module11 => User session control => user menu => made user logout, created user form login and style, made login logical and fixed some bugs

---
+++ modulo11/src/app/controllers/SessionController.js
@@ -0,0 +1,13 @@
+module.exports = {
+  loginForm(req, res) {
+    res.render("session/login");
+  },
+  login(req, res) {
+    req.session.userId = req.user.id;
+    return res.redirect("/users");
+  },
+  logout(req, res) {
+    req.session.destroy();
+    return res.redirect("/");
+  },
+};
