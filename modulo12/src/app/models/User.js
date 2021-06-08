193ba778441f28a59a2f7cc784fa2756ef1f3298
pedrolssilva <pedrolssilva@hotmail.com>
2021-06-07 21:02:36 -0300

    Module12 => Advanced NodeJS => Created Base Model and using Base Model

--- modulo12/src/app/models/User.js
+++ modulo12/src/app/models/User.js
@@ -1,94 +1,87 @@
-const db = require("../../config/db");
-const { hash } = require("bcryptjs");
-const fs = require("fs");
-const Product = require("../models/Product");
 const Base = require("./Base");
 
 Base.init({ table: "users" });
 
-const User = {
+module.exports = {
   ...Base,
-  async create(data) {
-    try {
-      const query = `
-    INSERT INTO users (
-      name,
-      email,
-      password,
-      cpf_cnpj,
-      cep,
-      address
-    ) VALUES ($1, $2, $3, $4, $5, $6)
-    RETURNING id
-  `;
-
-      const passwordHash = await hash(data.password, 8);
-
-      const values = [
-        data.name,
-        data.email,
-        passwordHash,
-        data.cpf_cnpj.replace(/\D/g, ""),
-        data.cep.replace(/\D/g, ""),
-        data.address,
-      ];
-
-      const results = await db.query(query, values);
-
-      return results.rows[0].id;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-  async update(id, fields) {
-    let query = "UPDATE users SET";
-
-    Object.keys(fields).map((key, index, array) => {
-      if (index + 1 < array.length) {
-        query = `${query}
-          ${key} = '${fields[key]}',
-        `;
-      } else {
-        query = `${query}
-          ${key} = '${fields[key]}'
-          WHERE id = ${id}
-        `;
-      }
-    });
-
-    await db.query(query);
-    return;
-  },
-  async delete(id) {
-    // pegar todos os produtos
-    let results = await db.query("SELECT * FROM products WHERE user_id=$1", [
-      id,
-    ]);
-    const products = results.rows;
-
-    // dos produtos, pegar todas as imagens
-    const allFilesPromise = products.map((product) =>
-      Product.files(product.id)
-    );
-
-    let promiseResults = await Promise.all(allFilesPromise);
-
-    // rodar a remoçao do usuario
-    await db.query("DELETE FROM users WHERE id=$1", [id]);
-
-    //remover as imagens da pasta public
-    promiseResults.map((results) => {
-      results.rows.map((file) => {
-        try {
-          fs.unlinkSync(file.path);
-        } catch (error) {
-          console.error(error);
-        }
-      });
-    });
-  },
 };
 
-console.log(User);
+//   async create(data) {
+//     try {
+//       const query = `
+//     INSERT INTO users (
+//       name,
+//       email,
+//       password,
+//       cpf_cnpj,
+//       cep,
+//       address
+//     ) VALUES ($1, $2, $3, $4, $5, $6)
+//     RETURNING id
+//   `;
 
-module.exports = User;
+//       const passwordHash = await hash(data.password, 8);
+
+//       const values = [
+//         data.name,
+//         data.email,
+//         passwordHash,
+//         data.cpf_cnpj.replace(/\D/g, ""),
+//         data.cep.replace(/\D/g, ""),
+//         data.address,
+//       ];
+
+//       const results = await db.query(query, values);
+
+//       return results.rows[0].id;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   },
+//   async update(id, fields) {
+//     let query = "UPDATE users SET";
+
+//     Object.keys(fields).map((key, index, array) => {
+//       if (index + 1 < array.length) {
+//         query = `${query}
+//           ${key} = '${fields[key]}',
+//         `;
+//       } else {
+//         query = `${query}
+//           ${key} = '${fields[key]}'
+//           WHERE id = ${id}
+//         `;
+//       }
+//     });
+
+//     await db.query(query);
+//     return;
+//   },
+//   async delete(id) {
+//     // pegar todos os produtos
+//     let results = await db.query("SELECT * FROM products WHERE user_id=$1", [
+//       id,
+//     ]);
+//     const products = results.rows;
+
+//     // dos produtos, pegar todas as imagens
+//     const allFilesPromise = products.map((product) =>
+//       Product.files(product.id)
+//     );
+
+//     let promiseResults = await Promise.all(allFilesPromise);
+
+//     // rodar a remoçao do usuario
+//     await db.query("DELETE FROM users WHERE id=$1", [id]);
+
+//     //remover as imagens da pasta public
+//     promiseResults.map((results) => {
+//       results.rows.map((file) => {
+//         try {
+//           fs.unlinkSync(file.path);
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       });
+//     });
+//   },
