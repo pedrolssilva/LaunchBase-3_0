b2fdb6122e4d788d174928544cb0b2b9d588f48f
pedrolssilva <pedrolssilva@hotmail.com>
2021-06-16 21:40:35 -0300

    Module12 => Advanced NodeJS => Created service to load products and used the service in the controllers

--- modulo12/src/app/controllers/HomeController.js
+++ modulo12/src/app/controllers/HomeController.js
@@ -1,39 +1,16 @@
-const Product = require("../models/Product");
-
-const { formatPrice } = require("../../lib/utils");
+const LoadProductSerivce = require("../services/LoadProductService");
 
 module.exports = {
   async index(req, res) {
-    const products = await Product.findAll();
-
-    if (!products) {
-      return res.send("Products not found!");
-    }
-
-    async function getImage(productId) {
-      let files = await Product.files(productId);
-      files = files.map(
-        (file) =>
-          `${req.protocol}://${req.headers.host}${file.path.replace(
-            "public",
-            ""
-          )}`
+    try {
+      const allProducts = await LoadProductSerivce.load("products");
+      const products = allProducts.filter((product, index) =>
+        index > 2 ? false : true
       );
 
-      return files[0];
+      return res.render("home/index", { products });
+    } catch (error) {
+      console.error(error);
     }
-
-    const productsPromise = products
-      .map(async (product) => {
-        product.img = await getImage(product.id);
-        product.oldPrice = formatPrice(product.old_price);
-        product.price = formatPrice(product.price);
-        return product;
-      })
-      .filter((product, index) => (index > 2 ? false : true));
-
-    const lastAdded = await Promise.all(productsPromise);
-
-    return res.render("home/index", { products: lastAdded });
   },
 };
--- modulo12/src/app/controllers/ProductController.js
+++ modulo12/src/app/controllers/ProductController.js
@@ -2,8 +2,7 @@ const { unlinkSync } = require("fs");
 const Category = require("../models/Category");
 const Product = require("../models/Product");
 const File = require("../models/File");
-
-const { formatPrice, date } = require("../../lib/utils");
+const LoadProductService = require("../services/LoadProductService");
 
 module.exports = {
   async create(req, res) {
@@ -64,38 +63,28 @@ module.exports = {
   },
   async show(req, res) {
     try {
-      const product = await Product.find(req.params.id);
+      const product = await LoadProductService.load("product", {
+        where: {
+          id: req.params.id,
+        },
+      });
+
       if (!product) {
         return res.send("Product not found!");
       }
 
-      const { day, hour, minutes, month } = date(product.updated_at);
-
-      product.published = {
-        day: `${day}/${month}`,
-        hour: `${hour}h${minutes}`,
-      };
-
-      product.oldPrice = formatPrice(product.old_price);
-      product.price = formatPrice(product.price);
-
-      let files = await Product.files(product.id);
-      files = files.map((file) => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`,
-      }));
-
-      return res.render("products/show", { product, files });
+      return res.render("products/show", { product });
     } catch (error) {
       console.error(error);
     }
   },
   async edit(req, res) {
     try {
-      const product = await Product.find(req.params.id);
+      const product = await LoadProductService.load("product", {
+        where: {
+          id: req.params.id,
+        },
+      });
 
       if (!product) {
         return res.send("Product not found");
@@ -104,20 +93,7 @@ module.exports = {
       // get categories
       const categories = await Category.findAll();
 
-      product.old_price = formatPrice(product.old_price);
-      product.price = formatPrice(product.price);
-
-      // get images
-      let files = await Product.files(product.id);
-      files = files.map((file) => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`,
-      }));
-
-      return res.render("products/edit", { product, categories, files });
+      return res.render("products/edit", { product, categories });
     } catch (error) {
       console.error(error);
     }
@@ -155,7 +131,7 @@ module.exports = {
       if (req.body.old_price != req.body.price) {
         const oldProduct = await Product.find(req.body.id);
 
-        req.body.old_price = oldProduct.rows[0].price;
+        req.body.old_price = oldProduct.price;
       }
 
       await Product.update(req.body.id, {
