0e932e3164b10a99f6e155a698e42e2a53b88be0
pedrolssilva <pedrolssilva@hotmail.com>
2021-07-15 20:21:13 -0300

    Module13 => Extra features => used features remove-one and delete from the cart and add empty-cart message and styled it

--- modulo13/src/app/controllers/CartController.js
+++ modulo13/src/app/controllers/CartController.js
@@ -1,4 +1,4 @@
-const { addOne } = require("../../lib/cart");
+const { addOne, removeOne } = require("../../lib/cart");
 const Cart = require("../../lib/cart");
 
 const LoadProductService = require("../services/LoadProductService");
@@ -34,6 +34,38 @@ module.exports = {
     req.session.cart = cart;
 
     // redirecionar o usuario para a tela do carrinho
+    return res.redirect("/cart");
+  },
+  removeOne(req, res) {
+    //pegar o id do produto
+    let { id } = req.params;
+
+    // pegar o carrinho da sessao
+    let { cart } = req.session;
+
+    // se nao tiver carrinho, retornar
+    if (!cart) {
+      return res.redirect("/cart");
+    }
+
+    // iniciar o carrinho (gerenciador de carrinho)
+    cart = Cart.init(cart).removeOne(id);
+
+    // atualizar o carrinho da sessao, removendo 1 item
+    req.session.cart = cart;
+
+    // redirecionamento para a pagina cart
+    return res.redirect("/cart");
+  },
+  delete(req, res) {
+    let { cart } = req.session;
+    let { id } = req.params;
+    if (!cart) {
+      return res.redirect("/cart");
+    }
+
+    req.session.cart = Cart.init(cart).delete(id);
+
     return res.redirect("/cart");
   },
 };
