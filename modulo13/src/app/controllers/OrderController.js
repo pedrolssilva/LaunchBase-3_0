974d12b233f4c637434b587f3dfa18208d772625
pedrolssilva <pedrolssilva@hotmail.com>
2021-07-23 20:11:54 -0300

    Module13 => Extra features => order data to be shown in the front

--- modulo13/src/app/controllers/OrderController.js
+++ modulo13/src/app/controllers/OrderController.js
@@ -4,6 +4,7 @@ const Order = require("../models/Order");
 
 const Cart = require("../../lib/cart");
 const mailer = require("../../lib/mailer");
+const { formatPrice, date } = require("../../lib/utils");
 
 const email = (seller, product, buyer) => `
   <h2> Olá ${seller.name}</h2>
@@ -23,6 +24,47 @@ const email = (seller, product, buyer) => `
 `;
 
 module.exports = {
+  async index(req, res) {
+    // pegar os pedidos do usuario
+    let orders = await Order.findAll({
+      where: { buyer_id: req.session.userId },
+    });
+
+    const getOrdersPromise = orders.map(async (order) => {
+      // detalhes do produto
+      order.product = await LoadProductSerivce.load("products", {
+        where: { id: order.product_id },
+      });
+
+      // detalhes do comprador
+      order.buyer = await User.findOne({ where: { id: order.buyer_id } });
+
+      // detalhes do vendedor
+      order.seller = await User.findOne({ where: { id: order.seller_id } });
+
+      // formataçao de preço
+      order.formattedPrice = formatPrice(order.price);
+      order.formattedTotal = formatPrice(order.total);
+
+      // formataçao do status
+      const statuses = {
+        open: "Aberto",
+        sold: "Vendido",
+        canceled: "Cancelado",
+      };
+
+      order.formattedStatus = statuses[order.status];
+
+      // formataçao de atualizado em ...
+      const updatedAt = date(order.updated_at);
+      order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ${updatedAt.hour}:${updatedAt.minutes}`;
+      return order;
+    });
+
+    orders = await Promise.all(getOrdersPromise);
+
+    return res.render("orders/index", { orders });
+  },
   async post(req, res) {
     try {
       // pegar os produtos do carrinho
@@ -72,6 +114,10 @@ module.exports = {
       });
 
       await Promise.all(createOrdersPromise);
+
+      // clean cart
+      delete req.session.cart;
+      Cart.init();
 
       // notificar o usuario com alguma mensagem de sucesso
       return res.render("orders/success");
